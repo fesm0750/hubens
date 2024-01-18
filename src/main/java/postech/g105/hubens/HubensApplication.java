80b997112808a9743220f5524f48524a8cfae8d3
Fernando Martins <emmanuel.martins@outlook.com>
2024-01-17 23:06:02 -0300

    Initial Commit

---
+++ src/main/java/postech/g105/hubens/HubensApplication.java
@@ -0,0 +1,13 @@
+package postech.g105.hubens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/postech/g105/hubens/HubensApplicationTests.java
@@ -0,0 +1,13 @@
+package postech.g105.hubens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
