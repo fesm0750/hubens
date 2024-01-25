f6ed5a6ae90e5a594c2e67116175d503127cd39a
Fernando Martins <emmanuel.martins@outlook.com>
2024-01-25 02:55:59 -0300

    fix: Corrige peristência de data publicação

--- src/main/java/postech/g105/hubens/HubensApplication.java
+++ src/main/java/postech/g105/hubens/HubensApplication.java
@@ -2,8 +2,10 @@ package postech.g105.hubens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
+@EnableReactiveMongoAuditing
 public class HubensApplication {
 
 	public static void main(String[] args) {
