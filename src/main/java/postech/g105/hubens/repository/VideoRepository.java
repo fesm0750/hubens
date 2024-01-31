e5717b50aa616668ea7491217a3a3405660defd7
Fernando Martins <emmanuel.martins@outlook.com>
2024-01-31 11:27:34 -0300

    style: VideoRepo formatação e comentários

--- src/main/java/postech/g105/hubens/repository/VideoRepository.java
+++ src/main/java/postech/g105/hubens/repository/VideoRepository.java
@@ -16,13 +16,17 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface VideoRepository extends ReactiveMongoRepository<Video, String> {
 
+    // filtra os vídeos por parâmetros opcionais: título, categoria e período de
+    // publicação
     @Query("{ $and: [ "
-    + "?#{ [0] != null ? { 'titulo': { $regex: [0], $options: 'i' } } : { $where: 'true' } }, "
-    + "?#{ [1] != null ? { 'categoria': { $eq: [1] } } : { $where: 'true' } }, "
-    + "?#{ [2] != null ? { 'dataPublicacao': { $gte: [2], $lte: [3] } } : { $where: 'true' } }"
-    + "] }")
-    Flux<Video> findFilteredAndPaged(String titulo, VideoCategoria categoria, LocalDate dataPublicacaoInicio, LocalDate dataPublicacaoFim, Pageable pageable);
+            + "?#{ [0] != null ? { 'titulo': { $regex: [0], $options: 'i' } } : { $where: 'true' } }, "
+            + "?#{ [1] != null ? { 'categoria': { $eq: [1] } } : { $where: 'true' } }, "
+            + "?#{ [2] != null ? { 'dataPublicacao': { $gte: [2], $lte: [3] } } : { $where: 'true' } }"
+            + "] }")
+    Flux<Video> findFilteredAndPaged(String titulo, VideoCategoria categoria, LocalDate dataPublicacaoInicio,
+            LocalDate dataPublicacaoFim, Pageable pageable);
 
-    @Query(value="{ 'id' : ?0 }",fields="{ 'categoria' : 1 }")
+    // Retorna a categoria de um video
+    @Query(value = "{ 'id' : ?0 }", fields = "{ 'categoria' : 1 }")
     Mono<VideoCategoriaDTO> findCategoriaById(String id);
 }
